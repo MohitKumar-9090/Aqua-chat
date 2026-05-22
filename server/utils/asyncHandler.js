708be9557df0ac0754f35eb3da7c140176a18803
Mohit Kumar <mohit.pandey@gmail.com>
2026-05-23 00:07:04 +0530

    removed mongodb added firebase backend

--- server/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-export const asyncHandler = (handler) => (req, res, next) => {
-  Promise.resolve(handler(req, res, next)).catch(next);
-};
-
-export const createError = (statusCode, message) => {
-  const error = new Error(message);
-  error.statusCode = statusCode;
-  return error;
-};
